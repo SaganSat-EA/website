be0ae72a67a49ed837cf4ee8025ff2078fff9467
Leonardo Dias Lissone <leonardo.lissonez@gmail.com>
2021-05-19 20:21:44 -0300

    add styled components and global style

---
+++ src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    --backgound: #F7F7F7;
+    --primary: #64a19d;
+    --secondary: #6c757d;
+    --gray-dark: #343a40;
+    --light: #f8f9fa;
+    --black: #000000;
+    --success: #28a745;
+    --info: #17a2b8;
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: "Nunito", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
