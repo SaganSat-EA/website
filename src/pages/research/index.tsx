6a213b895de5d46de247e17bf0f56e4ce5f732b8
Leonardo Dias Lissone <leonardo.lissonez@gmail.com>
2021-06-02 22:20:43 -0300

    alter folders components and styles pages

--- src/pages/research/index.tsx
+++ src/pages/research/index.tsx
@@ -1,9 +1,9 @@
-import { Header } from './Header'
-import { Beginning } from './Beginning'
-import { Development } from './Development'
-import { Members } from './Members'
+import { Header } from '../../components/Research/Header'
+import { Beginning } from '../../components/Research/Beginning'
+import { Development } from '../../components/Research/Development'
+import { Members } from '../../components/Research/Members'
 
-import { Container } from './styles'
+import { Container } from '../../styles/research'
 
 export default function Research() {
   return (
